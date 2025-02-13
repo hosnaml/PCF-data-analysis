20b927edaa5d2ce3bc46d233e1f44a70e91787c0
hosna <hosna@kth.se>
2025-02-13 15:40:54 +0100

    apple_desktops VS hp_desktops

--- apple_vs_others.py
+++ apple_vs_others.py
@@ -6,8 +6,20 @@ df = pd.read_csv('boavizta-data-us.csv')
 apple_laptops = df[(df['manufacturer'] == 'Apple') & (df['subcategory'] == 'Laptop')]
 hp_laptops = df[(df['manufacturer'] == 'HP') & (df['subcategory'] == 'Laptop')]
 
-apple_avg_emissions = apple_laptops['gwp_total'].mean()
-hp_avg_emissions = hp_laptops['gwp_total'].mean()
+apple_laptop_avg_emissions = apple_laptops['gwp_total'].mean()
+hp_laptop_avg_emissions = hp_laptops['gwp_total'].mean()
 
-print(f"Average emissions for Apple laptops: {apple_avg_emissions} kg CO₂")
-print(f"Average emissions for HP laptops: {hp_avg_emissions} kg CO₂")
+print(f"Average emissions for Apple laptops: {apple_laptop_avg_emissions} kg CO₂")
+print(f"Average emissions for HP laptops: {hp_laptop_avg_emissions} kg CO₂")
+
+
+apple_desktops = df[(df['manufacturer'] == 'Apple') & (df['subcategory'] == 'Desktop')]
+hp_desktops = df[(df['manufacturer'] == 'HP') & (df['subcategory'] == 'Desktop')]
+
+apple_desktop_avg_emissions = apple_desktops['gwp_total'].mean()
+hp_desktop_avg_emissions = hp_desktops['gwp_total'].mean()
+
+print(f"Average emissions for Apple laptops: {apple_desktop_avg_emissions} kg CO₂")
+print(f"Average emissions for HP laptops: {hp_desktop_avg_emissions} kg CO₂")
+
+
