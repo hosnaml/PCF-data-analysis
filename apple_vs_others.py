5e84277155c32984b08d19c4a4132bdc73b636c2
hosna <hosna@kth.se>
2025-02-13 15:34:34 +0100

    apple laptops VS hp latops

--- apple_vs_others.py
+++ apple_vs_others.py
@@ -1,4 +1,13 @@
-import csv
+import pandas as pd
 
-with open('boavizta-data-us.csv') as csv_file:
-    reader = csv.reader(csv_file)
+df = pd.read_csv('boavizta-data-us.csv')
+
+# filtering dataframes with apple manufacturer and laptop as type.
+apple_laptops = df[(df['manufacturer'] == 'Apple') & (df['subcategory'] == 'Laptop')]
+hp_laptops = df[(df['manufacturer'] == 'HP') & (df['subcategory'] == 'Laptop')]
+
+apple_avg_emissions = apple_laptops['gwp_total'].mean()
+hp_avg_emissions = hp_laptops['gwp_total'].mean()
+
+print(f"Average emissions for Apple laptops: {apple_avg_emissions} kg CO₂")
+print(f"Average emissions for HP laptops: {hp_avg_emissions} kg CO₂")
