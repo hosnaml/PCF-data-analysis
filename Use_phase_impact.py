e79e157abfb29b497cbb350903278b8ab3a0f879
hosna <hosna@kth.se>
2025-02-28 12:09:44 +0100

    Adverage CO2 in usage

--- Use_phase_impact.py
+++ Use_phase_impact.py
@@ -5,8 +5,11 @@ num_rows = len(df) - 1
 total_gwp_use_ratio = df['gwp_use_ratio'].sum()
 total_gwp = df['gwp_total'].sum()
 total_gwp_use = total_gwp * total_gwp_use_ratio
-gwp_use_percentage = ((total_gwp_use / total_gwp) *100 ) / num_rows
+adverage_gwp_use = total_gwp_use / num_rows
+adverage_gwp_total = total_gwp / num_rows
+adverage_gwp_use_percentage = ((total_gwp_use / total_gwp) *100 ) / num_rows
 
-print(f"Total GWP Use Ratio for all products: {total_gwp_use}")
-print(f"Total GWP for all products: {total_gwp}")
-print(f"Percentage of use phase across different product categories: {gwp_use_percentage: .2f}%")
+print(f"ratio: {total_gwp_use_ratio}")
+print(f"Adverage GWP Usage for all products: {adverage_gwp_use}")
+print(f"Advergae GWP for all products: {adverage_gwp_total}")
+print(f"Percentage of use phase across different product categories: {adverage_gwp_use_percentage: .2f}%")
